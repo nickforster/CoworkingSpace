184c05e8063259647c158db04665d6dab07dbba1
nickforster <nick_forster@icloud.com>
2023-07-13 16:23:00 +0200

    finalized springdoc

--- src/main/java/ch/zli/m223/ksh20/user/init/ServerInit.java
+++ src/main/java/ch/zli/m223/ksh20/user/init/ServerInit.java
@@ -1,7 +1,9 @@
 package ch.zli.m223.ksh20.user.init;
 
+import ch.zli.m223.ksh20.user.model.Reservation;
 import ch.zli.m223.ksh20.user.model.User;
 import ch.zli.m223.ksh20.user.model.enums.Role;
+import ch.zli.m223.ksh20.user.repository.ReservationRepository;
 import ch.zli.m223.ksh20.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -15,11 +17,11 @@ public class ServerInit implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        userRepository.insertUser(new User("Max", "SI-UU", "max@siu.ch", "asd34454"));
-        userRepository.insertUser(new User("OmegaMax", "tax", "dajfha@gmail.com", "dhjfaadf"));
         User u = new User("admin", "admin", "admin", "admin");
         u.setRole(Role.ADMIN);
         userRepository.insertUser(u);
+        userRepository.insertUser(new User("Max", "SI-UU", "max@siu.ch", "asd34454"));
+        userRepository.insertUser(new User("OmegaMax", "tax", "dajfha@gmail.com", "dhjfaadf"));
 
     }
 }
